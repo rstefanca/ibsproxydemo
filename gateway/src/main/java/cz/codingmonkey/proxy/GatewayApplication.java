4ba5f385970e21bc5102ac3a5fbb428b858f2931
Richard Stefanca <rstefanca@gmail.com>
2017-04-16 16:08:52 +0200

    eureka discovery service

--- gateway/src/main/java/cz/codingmonkey/proxy/GatewayApplication.java
+++ gateway/src/main/java/cz/codingmonkey/proxy/GatewayApplication.java
@@ -5,6 +5,8 @@ import cz.codingmonkey.proxy.filters.pre.LogFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -15,6 +17,8 @@ import org.springframework.web.client.RestTemplate;
 
 @EnableZuulProxy
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaClient
 public class GatewayApplication {
 
 	public static void main(String[] args) {
@@ -35,5 +39,10 @@ public class GatewayApplication {
 	public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
 		return restTemplateBuilder.build();
 	}
+
+	/*@Bean
+	public SendResponseFilter sendResponseFilter() {
+		return new SendResponseFilter();
+	}*/
 
 }
--- utility-payment/src/main/java/cz/codingmonkey/utiltitypayment/UtilityPaymentApplication.java
+++ utility-payment/src/main/java/cz/codingmonkey/utiltitypayment/UtilityPaymentApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableEurekaClient
 public class UtilityPaymentApplication {
 
 	public static void main(String[] args) {
